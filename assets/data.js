b49794d26e38912d80c326a9e20b149e72e65c63
xwf <shangguanqingyun@baoufu.com>
2025-08-29 17:36:55 +0800

    feat: 更改渲染方式

--- assets/data.js
+++ assets/data.js
@@ -1,16 +1,28 @@
 // 声明并导出剧集数据
 const seriesData = {
-  "A": [
-    { name: "安多 S01-S02", url: "https://pan.quark.cn/s/203a8b68dee4" },
-    { name: "傲骨之战 S01-S06", url: "https://pan.quark.cn/s/daf47084fa1b" },
-    { name: "暗黑 全集S01-S03 4K", url: "https://pan.quark.cn/s/19e79a8aeb13" }
+  A: [
+    {
+      name: "安多 S01-S02",
+      url: "https://pan.quark.cn/s/203a8b68dee4",
+    },
+    {
+      name: "傲骨之战 S01-S06",
+      url: "https://pan.quark.cn/s/daf47084fa1b",
+    },
+    {
+      name: "暗黑 全集S01-S03 4K",
+      url: "https://pan.quark.cn/s/19e79a8aeb13",
+    },
   ],
-  "B": [
-    { name: "不死法医", url: "https://pan.quark.cn/s/235f04485c7f" }
+  B: [
+    { name: "不死法医", url: "https://pan.quark.cn/s/235f04485c7f" },
   ],
   // 其他字母的数据...
-  "J": [
-    { name: "绝命毒师 (Breaking Bad)", url: "https://example.com/download/breaking_bad" }
+  J: [
+    {
+      name: "绝命毒师 (Breaking Bad)",
+      url: "https://example.com/download/breaking_bad",
+    },
   ],
   // 继续添加其他字母的数据...
 };
